539fdaa67e0e2378f7a7baece60744e4f47c0022
Pal <vishal.a.pal@capgemini.com>
2023-06-10 15:37:15 +0530

    initial build pushed

---
+++ src/main/java/com/vishalpal555/aplication101/Aplication101Application.java
@@ -0,0 +1,13 @@
+package com.vishalpal555.aplication101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aplication101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aplication101Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vishalpal555/aplication101/Aplication101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishalpal555.aplication101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aplication101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
